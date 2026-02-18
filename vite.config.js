6c757b3a0e5347a15325acd5915c99e254b97585
Manikandan K <manikandan.dev.it@gmail.com>
2026-02-18 13:41:51 +0530

    12:37

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/',   // IMPORTANT
+  plugins: [react(), tailwindcss()],
+  base: '/'
 })
