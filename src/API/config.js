db064d6c07dec3447767bf5e5588d353e8d832c8
Manikandan K <manikandan.dev.it@gmail.com>
2026-02-19 10:56:21 +0530

    Fix frontend tracking

--- src/API/config.js
+++ src/API/config.js
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    BASE_URL: import.meta.env.VITE_API_URL || "https://try2-2-mpi9.onrender.com"
+    BASE_URL: import.meta.env.VITE_API_URL || "https://synquote-backend.onrender.com/"
   };
